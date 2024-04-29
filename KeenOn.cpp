e2275134f4982022aba7e306f054ee55198dc82c
KDK2 <kdk9678@gmail.com>
2024-04-29 21:36:38 +0900

    Successful build

--- KeenOn.cpp
+++ KeenOn.cpp
@@ -338,7 +338,7 @@ void KeenOn::onROSReceiveVel(geometry_msgs::Twist &msg, void *pArg)
 {
     KeenOn *ko =(KeenOn*)pArg;
     qDebug()<<msg.linear.x << msg.angular.z;
-    //ko->Go(msg.linear.x,msg.angular.z);
+    ko->Go(msg.linear.x,msg.angular.z);
 }
 
 #include "3d/dhMat.h"
